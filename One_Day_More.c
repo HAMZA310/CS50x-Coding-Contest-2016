826f14cd6238a457d1c7f00895d16a396c1485f9
HAMZA310 <hamzaliaqet@gmail.com>
2016-08-01 16:26:46 +0500

    Update One_Day_More.c

--- One_Day_More.c
+++ One_Day_More.c
@@ -1,23 +1,35 @@
-#include <stdio.h>
+/**
+ * Write a program that, given a year, Y, as an integer via standard input,
+ * prints via standard output Leap Year if Y is a leap year, and Not Leap Year if Y is not a leap year.
+ * Recall that:
+ * A year is a leap year if it is a multiple of four (2004, 2008, and 2012 are all leap years).
+ * The exception to this rule is that a year which is a multiple of 100
+ * and also not a multiple of 400 is not a leap year. For example, 1800 and 1900 are not leap years,
+ * but 2000 is a leap year (because it is a multiple of 400).
+ **/ 
 
+
+
+#include <stdio.h>
 #include "cs50.h"
 
 int main(void)
 {
     int n = GetInt ();
- if(n%100 == 0)
- {
-     if (n%400 == 0)
-     printf("Leap Year\n");
-     else 
-     printf("Not Leap Year\n");
+    if (n%100 == 0)
+         {
+            if (n%400 == 0)
+            printf("Leap Year\n");
      
- }
+            else 
+            printf("Not Leap Year\n");
+         }
+         
+         
+   
     else if (n%4 == 0)
-{
-         printf("Leap Year\n");
+    printf("Leap Year\n");
 
-}    
     else 
     printf("Not Leap Year\n");
 
