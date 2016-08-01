120c77c5bc6c3ca31bbc7ca5167e4814722c46af
HAMZA310 <hamzaliaqet@gmail.com>
2016-08-01 16:44:26 +0500

    Update punctuation.c

--- punctuation.c
+++ punctuation.c
@@ -1,8 +1,16 @@
+/**
+ * Write a program that, given a line of text, T, via standard input,
+ * prints it to standard output with the following modifcations:
+ * After every period (.) and comma (,), a space must be added.
+ * The first letter after every period must be capitalized.
+ **/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
 
 #include "cs50.h"
 
@@ -11,32 +19,25 @@ int main(void)
     string s = GetString();
     
     for ( int x = 0, n = strlen (s); x < n; x++)
-   {
-
-         printf("%c", s[x]);
-     if (s[x] == '.')
-            {
-              
+        {
+            printf("%c", s[x]);
             
-              if ((s[(x+1)] <=122) && (s[(x+1)] >=97))
-               s[x + 1] = s[x+ 1] -32;
-               else 
-               s[x+1] = s[x+1];
+            if (s[x] == '.')
+            {
+                if ((s[(x+1)] <=122) && (s[(x+1)] >=97))  //if lowercase, capitalizing...
+                s[x + 1] = s[x+ 1] -32;
+               
+                else 
+                s[x+1] = s[x+1];
             }
      
-     if ((s[x] == ',') || s[x] == '.')
-         {
-           // isupper (s[x+1]);
+     
+            if ((s[x] == ',') || s[x] == '.')
             printf(" ");
+                
+        }
         
-         }
-       
-   
-       
-   }
-       
-         printf("\n");
+        printf("\n");
     
     
-
 }
