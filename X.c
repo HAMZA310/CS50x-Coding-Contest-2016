11e032e2d57fda746a7424fedff6f8386c43dfae
HAMZA310 <hamzaliaqet@gmail.com>
2016-08-01 16:34:39 +0500

    Update X.c

--- X.c
+++ X.c
@@ -1,3 +1,19 @@
+/**
+ * PROBLEM DESCRIPTION:
+ * 
+ * Write a program that, given an odd integer, N, in [3, 23],
+ * draws a box (out of * characters) of height N and width N with an X in the middle.
+ * That is, the diagonals of the box should be drawn with * characters.
+ * For example, 5 should give you:
+ *   ***** 
+ *   ** **  
+ *   * * *  
+ *   ** **  
+ *   *****
+ **/
+
+
+
 #include<stdio.h>
 #include<cs50.h>
 
@@ -12,22 +28,22 @@ int main ()
 
 
     for  (int k = 0; k < n; k++)
-    {
+         {
+             for (int i = 0; i < n; i++)
+             {
+                if ((k != 0) && (k != n-1) && (i != 0) && (i != n-1))
+                {
+                     if ((k != i) && ((n - i) != (k + 1)))
+                     {
+                      printf(" ");
+                      continue;
+                      }
+                }
+                printf("*");
+             }           
+
+
+         printf("\n");
+        }   
         
-    for (int i = 0; i < n; i++)
-  {
-      if ((k != 0) && (k != n-1) && (i != 0) && (i != n-1))
-      {
-      if ((k != i) && ((n - i) != (k + 1)))
-      {
-          printf(" ");
-          continue;
-      }
-      }
-      printf("*");
-    }
-  printf("\n");
-  
-  
-}
 }
