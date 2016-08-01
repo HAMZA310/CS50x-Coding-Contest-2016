198fc1df603212843fe761c1f24b4e0babcd1d7b
HAMZA310 <hamzaliaqet@gmail.com>
2016-08-01 16:13:24 +0500

    Update Name_your_cat.c

--- Name_your_cat.c
+++ Name_your_cat.c
@@ -1,7 +1,18 @@
+/**
+ * Problem Description:
+ * Between the words cat and kitten, there are seven unique characters (a, c, e, i, k, n, and t),
+ * because only those characters appear in the two words.
+ * Given a string (i.e., a cat's name), reverse all instances of those characters,
+ * while leaving the other 19 letters in the alphabet as they are.
+ * Each of those 19 characters, if they appear in the name, must remain at the same index.
+ *If there is only one character to reverse, leave it in the same location.
+**/
+
+
 #include<stdio.h>
 #include<cs50.h>
 #include<string.h>
-#define num strlen(s)
+
 int main ()
 {
     int num = GetInt ();
@@ -10,32 +21,23 @@ int main ()
   
     for (int a = 0; a < num; a++ )
     {
-        
-        //int b = num -1;
        for (int b = k - 1; b > a; b--)
        {
-       
-        
-if (((s[a] == 'a') || (s[a] == 'c') || (s[a] == 'e') || (s[a] == 'i') || (s[a] == 'k') || (s[a] == 'n') || (s[a] == 't'))   &&    ((s[b] == 'a') || (s[b] == 'c') || (s[b] == 'e') || (s[b] == 'i') || (s[b] == 'k') || (s[b] == 'n') || (s[b] == 't')))
+            if (((s[a] == 'a') || (s[a] == 'c') || (s[a] == 'e') || (s[a] == 'i') || (s[a] == 'k') || (s[a] == 'n') || (s[a] == 't'))   &&    ((s[b] == 'a') || (s[b] == 'c') || (s[b] == 'e') || (s[b] == 'i') || (s[b] == 'k') || (s[b] == 'n') || (s[b] == 't')))
             {
                 char temp = s[a];
                 s[a] = s[b];
                 s[b] = temp;
                 
                 k = b;
-               
                 break;
             } 
-            
-            
-       }
-       
-        
-        
-        
-        
-       
+     
+        }
+      
     }
+    
+    //printing... 
     for (int g = 0, m = strlen(s); g < m; g++)
     printf("%c", s[g]);
     printf("\n");
